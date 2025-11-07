010234ec3344aecf9aa7ea7d276387fffdd2ac24
liencis <lienezegele@gmail.com>
2025-11-07 18:43:23 +0200

    Profile page all features added.

--- Pepper/helper.js
+++ Pepper/helper.js
@@ -3,3 +3,9 @@ export const validateEmail = (email) => {
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   );
 };
+
+export const validatePhoneNumber = (number) => {
+  const reg = /^[0]?[789]\d{9}$/;
+  return reg.test(number);
+};
+

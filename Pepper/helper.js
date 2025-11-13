78d0687bf06baef43660c0e79940bf699acf262b
liencis <lienezegele@gmail.com>
2025-11-13 18:38:41 +0200

    Horizontal scrool view with section filter.

--- Pepper/helper.js
+++ Pepper/helper.js
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+
 export const validateEmail = (email) => {
   return email.match(
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
@@ -88,4 +90,16 @@ export function getSectionListData(data) {
     }
   }
   return sectionListData;
+}
+
+export function useUpdateEffect(effect, dependencies = []) {
+  const isInitialMount = useRef(true);
+
+  useEffect(() => {
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+    } else {
+      return effect();
+    }
+  }, dependencies);
 }
